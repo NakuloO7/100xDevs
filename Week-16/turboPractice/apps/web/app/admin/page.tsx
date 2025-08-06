7ac17f201b725035139035853bf9a50413e4bfda
Turbobot <turbobot@vercel.com>
2025-08-06 23:04:03 +0530

    feat(create-turbo): apply official-starter transform

---
+++ Week-16/turboPractice/apps/web/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import { Admin } from "@repo/ui/admin"
+
+export default function Adminpage(){
+    return (
+        <div>Hello Hello below is the admin page
+        <Admin/>
+        </div>
+    )
+}
