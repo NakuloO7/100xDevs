a89454f883b1210823c9486197c7c6faee8117a4
Nakul <nakulgite08@gmail.com>
2026-01-11 16:31:12 +0530

    completed

---
+++ week-19/redis-pubsub/express-server/dist/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import { createClient } from 'redis';
+const app = express();
+app.use(express.json());
+const client = createClient();
+client.on('error', err => console.log('redis client erroo', err));
+app.post('/submit', async (req, res) => {
+    const problemId = req.body.problemId;
+    const code = req.body.code;
+    const language = req.body.language;
+    try {
+        await client.lPush("problems", JSON.stringify({ code, language, problemId }));
+        res.status(200).send("Submission received and stored");
+    }
+    catch (error) {
+        console.error("redis error", error);
+        res.status(500).send("Failed to store submission");
+    }
+});
+const startServer = async () => {
+    try {
+        await client.connect();
+        console.log("Connected to redis");
+        app.listen(3000, () => {
+            console.log("app listening on PORT : 3000");
+        });
+    }
+    catch (error) {
+        console.error("Failed to connect redis", error);
+    }
+};
+startServer();
+//# sourceMappingURL=index.js.map
